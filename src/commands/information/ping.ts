4e247f1b678a10d859bc80eb14a7879f2f022b93
ToastedToast <hey@toasted.dev>
2024-04-10 19:27:40 +0800

    feat(commands): add actual ping command

--- src/commands/information/ping.ts
+++ src/commands/information/ping.ts
@@ -1,11 +1,37 @@
-import { SlashCommandBuilder } from "discord.js";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import type { Command } from "@/structures/command";
+import config from "config";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pings the bot."),
-  run: ({ interaction }) => {
-    interaction.reply("Pong!");
+  run: async ({ client, interaction }) => {
+    const res = await interaction.deferReply({
+      ephemeral: true,
+      fetchReply: true,
+    });
+
+    const ping = res.createdTimestamp - interaction.createdTimestamp;
+
+    await interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("Pong! 🏓")
+          .addFields(
+            {
+              name: "🤖 Bot",
+              value: `${ping}ms`,
+              inline: true,
+            },
+            {
+              name: "📡 API",
+              value: `${client.ws.ping}ms`,
+              inline: true,
+            },
+          )
+          .setColor(config.colors.primary),
+      ],
+    });
   },
 } satisfies Command;
