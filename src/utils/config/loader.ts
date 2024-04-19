fc39dd66ad4e13ab6a1f3ef1c8ddb9d2728e1223
ToastedToast <hey@toasted.dev>
2024-04-19 15:32:07 +0800

    fix(config-loader): ignore error

--- src/utils/config/loader.ts
+++ src/utils/config/loader.ts
@@ -3,6 +3,8 @@ import { exists } from "fs/promises";
 
 export async function loadConfig(): Promise<Config> {
   if (await exists("./config.ts")) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const config = await import("../../../config").then((x) => x?.default);
     return config;
   }
