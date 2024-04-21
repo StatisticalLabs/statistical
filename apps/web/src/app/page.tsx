dec554ef08e644613d27d8b1a7e66a686e53c33b
ToastedToast <hey@toasted.dev>
2024-04-21 11:32:07 +0800

    add landing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,79 @@
+import Image from "next/image";
+import subscriberUpdate from "../../public/update.png";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { siteConfig } from "@/config/site";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center py-6 container">
+        <div className="flex flex-col items-center justify-center text-center gap-2">
+          <h1 className="text-balance text-5xl font-bold tracking-tight max-w-lg w-full">
+            Track your favorite channels{" "}
+            <span className="text-green-500 relative whitespace-nowrap italic">
+              in realtime
+              <SquigglyLines />
+            </span>
+          </h1>
+          <p className="text-muted-foreground text-xs sm:text-sm md:text-base text-pretty w-full max-w-2xl">
+            Get immediate updates when your favorite channel gets a new
+            subscriber update.
+          </p>
+          <div className="flex items-center gap-2">
+            <Link
+              href={siteConfig.links.invite}
+              target="_blank"
+              className={buttonVariants()}
+            >
+              Invite to your server
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              className={buttonVariants({ variant: "outline" })}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="0.97em"
+                height="1em"
+                viewBox="0 0 496 512"
+                className="mr-2 h-5 w-5"
+              >
+                <path
+                  fill="currentColor"
+                  d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6c-3.3.3-5.6-1.3-5.6-3.6c0-2 2.3-3.6 5.2-3.6c3-.3 5.6 1.3 5.6 3.6m-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9c2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3m44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9c.3 2 2.9 3.3 5.9 2.6c2.9-.7 4.9-2.6 4.6-4.6c-.3-1.9-3-3.2-5.9-2.9M244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2c12.8 2.3 17.3-5.6 17.3-12.1c0-6.2-.3-40.4-.3-61.4c0 0-70 15-84.7-29.8c0 0-11.4-29.1-27.8-36.6c0 0-22.9-15.7 1.6-15.4c0 0 24.9 2 38.6 25.8c21.9 38.6 58.6 27.5 72.9 20.9c2.3-16 8.8-27.1 16-33.7c-55.9-6.2-112.3-14.3-112.3-110.5c0-27.5 7.6-41.3 23.6-58.9c-2.6-6.5-11.1-33.3 2.6-67.9c20.9-6.5 69 27 69 27c20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27c13.7 34.7 5.2 61.4 2.6 67.9c16 17.7 25.8 31.5 25.8 58.9c0 96.5-58.9 104.2-114.8 110.5c9.2 7.9 17 22.9 17 46.4c0 33.7-.3 75.4-.3 83.6c0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252C496 113.3 383.5 8 244.8 8M97.2 352.9c-1.3 1-1 3.3.7 5.2c1.6 1.6 3.9 2.3 5.2 1c1.3-1 1-3.3-.7-5.2c-1.6-1.6-3.9-2.3-5.2-1m-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9c1.6 1 3.6.7 4.3-.7c.7-1.3-.3-2.9-2.3-3.9c-2-.6-3.6-.3-4.3.7m32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2c2.3 2.3 5.2 2.6 6.5 1c1.3-1.3.7-4.3-1.3-6.2c-2.2-2.3-5.2-2.6-6.5-1m-11.4-14.7c-1.6 1-1.6 3.6 0 5.9c1.6 2.3 4.3 3.3 5.6 2.3c1.6-1.3 1.6-3.9 0-6.2c-1.4-2.3-4-3.3-5.6-2"
+                />
+              </svg>
+              <p>View on GitHub</p>
+            </Link>
+          </div>
+        </div>
+        <div className="p-3" />
+        <div className="relative mx-auto">
+          <span className="absolute -inset-0.5 bg-[#323339] blur-lg -z-10" />
+          <div className="relative overflow-hidden rounded-lg bg-[#323339]">
+            <Image
+              src={subscriberUpdate}
+              placeholder="blur"
+              alt="Subscriber Update"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function SquigglyLines() {
+  return (
+    <svg
+      aria-hidden="true"
+      viewBox="0 0 418 42"
+      className="absolute left-0 top-2/3 h-[0.48em] w-full fill-green-500/60"
+      preserveAspectRatio="none"
+    >
+      <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+    </svg>
+  );
 }
