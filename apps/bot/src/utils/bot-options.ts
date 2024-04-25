e2f1915fbd19053f853f42fbecf8286dafb5fea7
ToastedToast <hey@toasted.dev>
2024-04-25 12:23:59 +0800

    feat(bot): status

--- apps/bot/src/utils/bot-options.ts
+++ apps/bot/src/utils/bot-options.ts
@@ -1,7 +1,19 @@
 import { botEnv as env } from "@statistical/env/bot";
-import { GatewayIntentBits, type ClientOptions } from "discord.js";
+import {
+  GatewayIntentBits,
+  type ClientOptions,
+  ActivityType,
+} from "discord.js";
 
 export const botOptions = {
   intents: [GatewayIntentBits.Guilds],
   shards: env.NODE_ENV === "production" ? "auto" : undefined,
+  presence: {
+    activities: [
+      {
+        name: "subscriber updates",
+        type: ActivityType.Listening,
+      },
+    ],
+  },
 } satisfies ClientOptions;
